0661a965d071889b9bbefcabb8c0f889a3a79952
ande4694 <knudsen879@hotmail.com>
2019-11-28 11:55:43 +0100

    Api klar

---
+++ API/ExamAPI/ExamAPI/Data/SeedData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using ExamAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamAPI.Data
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ExamAPIContext(serviceProvider.GetRequiredService<DbContextOptions<ExamAPIContext>>()))
+            {
+                if (context.Animal.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Animal.AddRange(
+                    new Animal
+                    {
+                        AnimalName = "Parrot"
+                    },
+                    new Animal
+                    {
+                        AnimalName = "Dog"
+                    },
+                    new Animal
+                    {
+                        AnimalName = "Cat"
+                    }
+                );
+                //context.SaveChanges();
+
+
+                if (context.Product.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                context.Product.AddRange(
+                    new Product
+                    {
+                        Name = "Ystads Pappegoya TJUBANG",
+                        Price = 13.6,
+                        Weight = 20,
+                        FoodForID = 1
+                    }
+
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ API/ExamAPI/ExamAPI/Models/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamAPI.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string AnimalName { get; set; }
+        List<Product> products { get; set; }
+
+        public Animal() { }
+    }
+}
