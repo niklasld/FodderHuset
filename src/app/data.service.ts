084314dc22085b14a9db7df40e0f89fb1158d563
niklasld <36439309+niklasld@users.noreply.github.com>
2019-12-04 18:56:24 +0100

    wolla

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import { cart } from './../entities/cart';
-import { Injectable } from '@angular/core';
-=======
 import { animal } from './../entities/animal';
->>>>>>> API
 import { product } from 'src/entities/product';
 import { ApiService } from './api.service';
 import { Injectable, OnInit } from '@angular/core';
@@ -14,59 +9,6 @@ import { User } from 'src/entities/user';
   providedIn: 'root'
 })
 export class DataService {
-<<<<<<< HEAD
-  testProducts: product[] = [
-    {
-      AnimalId: 1, 
-      Name: 'Hundebiks',
-      Animal: 'Hund',
-      Price: 230,
-      Weight: 5000
-    },
-    {
-      AnimalId: 2, 
-      Name: 'Kattehaps',
-      Animal: 'Kat',
-      Price: 130,
-      Weight: 1300
-    },
-    {
-      AnimalId: 3, 
-      Name: 'Hamsterfusk',
-      Animal: 'Hamster',
-      Price: 89,
-      Weight: 800
-    }
-  ];
-  testCart: cart[] = [
-    {
-      cartId: 1,
-      userId: 1,
-      productId: 1,
-      amount: 2
-    },
-    {
-      cartId: 2,
-      userId: 1,
-      productId: 2,
-      amount: 3
-    }
-  ];
-  constructor() { }
-
-  addProductToCart(userId: number, productId: number, amount: number) {
-    length = this.testCart.length;
-    let id = Math.floor(Math.random() * (999999 - 100000)) + 100000;
-    let newCartItem: cart = {
-      cartId: id,
-      userId: userId,
-      productId: productId,
-      amount: amount
-    };
-
-    this.testCart.push(newCartItem);
-  }
-=======
 
   users: User[];
   products: product[];
@@ -96,5 +38,4 @@ export class DataService {
   }
 
 
->>>>>>> API
 }
